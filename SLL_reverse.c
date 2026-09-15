b4f5a8220052c22a6c7c6bbef4e679d8daffca79
kritika28007 <kritikagupta0707@gmail.com>
2026-09-15 21:44:33 +0530

    Reverse of LL

---
+++ SLL_reverse.c
@@ -0,0 +1,54 @@
+//Length of Linked List
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+struct node*temp,*head,*new,*prev,*curr,*next;
+
+int main(){
+    int ch;
+    do{
+        new=malloc(sizeof(struct node));
+        printf("Enter the data: ");
+        scanf("%d", &new->data);
+        new->next=NULL;
+        if(head==NULL){
+            head=temp=new;
+        }
+        else{
+            temp->next=new;
+            temp=temp->next;
+        }
+        printf("Press 1 to continue: ");
+        scanf("%d", &ch);
+    }while(ch==1);
+    //original LL
+    printf("Original LL\n");
+    temp=head;
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    //reversed Logic
+    curr=head;
+    while(curr!=NULL){
+        next=curr->next;
+        curr->next=prev;
+
+        prev=curr;
+        curr=next;
+    }
+
+    printf("\nReversed LL\n");
+    head=temp=prev;
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    
+    return 0;
+}
